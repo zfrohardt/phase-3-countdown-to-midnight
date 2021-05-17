e7bd528947241138d8f30fee6b3dd4824251bced
Zach Frohardt <zfrohardt@gmail.com>
2021-05-17 10:35:57 -0700

    commit tree 8892bf50a14a519327e3225ff029180b31bcd93c

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,18 @@
 #write your code here
 
-def countdown
+def countdown(n)
+    while n > 0
+        puts "#{n} SECOND(S)!"
+        n -= 1
+    end
+    "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(n)
+    while n > 0
+        puts "#{n} SECOND(S)!"
+        n -= 1
+        sleep 1
+    end
+    "HAPPY NEW YEAR!"
 end
